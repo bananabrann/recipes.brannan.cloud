1c41a4dd8787f1bf6925f01da31648f23f4bf30c
bananabrann <plbrannan95@gmail.com>
2023-08-25 12:44:47 -0500

    Add burger recipe

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
 <h1>Recipes, Yum!</h1>
 <p>World class recipes on a world class website.</p>
+
+<h2>🦅 American</h2>
+<ul>
+  <li><a href="/american/burger">Greasy Hamburger</a></li>
+</ul>
+
+<h2>🏯 Japanese</h2>
+<i>Stay tuned for more recipes!</i>
+
+<h2>🤠 Mexican</h2>
+<i>Stay tuned for more recipes!</i>
---
+++ src/routes/american/burger/+page.svelte
@@ -0,0 +1,39 @@
+<h1>Greasy Hamburger</h1>
+
+<img
+  src="https://files.brannan.cloud/recipes/burger.png"
+  alt="A yummy, but definitely greasy, burger."
+/>
+
+<p>
+  Prepare to embark on a journey that celebrates the quintessential American comfort - the juicy,
+  greasy burger. Our recipe website is a treasure trove for burger enthusiasts, a place where
+  culinary delight takes the form of towering beef patties, melted cheese cascades, and crispy bacon
+  symphonies. From classic renditions that pay homage to the nostalgia of roadside diners, to
+  creative twists that push the boundaries of flavor, we're here to guide you step-by-step through
+  crafting your own masterpiece. So, roll up your sleeves and unleash your inner burger artist,
+  because the world of savory satisfaction awaits - one mouthwatering bite at a time.
+</p>
+
+<h2>Ingredients</h2>
+<ul>
+  <li>1 pound ground beef</li>
+  <li>1/2 teaspoon salt</li>
+  <li>1/2 teaspoon pepper</li>
+  <li>4 slices cheese</li>
+  <li>4 hamburger buns</li>
+  <li>4 tablespoons butter</li>
+  <li>4 tablespoons mayonnaise</li>
+  <li>4 tablespoons ketchup</li>
+</ul>
+
+<h2>Instructions</h2>
+<ol>
+  <li>Preheat grill to medium-high heat.</li>
+  <li>Divide ground beef into 4 equal portions and form into patties.</li>
+  <li>Season both sides of each patty with salt and pepper.</li>
+  <li>Grill burgers for 4-5 minutes per side, or until desired doneness.</li>
+  <li>Top each burger with a slice of cheese during the last minute of cooking.</li>
+  <li>Spread butter on cut sides of hamburger buns and toast on the grill.</li>
+  <li>Spread mayonnaise and ketchup on toasted buns and top with burgers.</li>
+</ol>
