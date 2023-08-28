c6184c336c9a97ac5d123a6b100e4a6f6e3dc9af
Amy-Hailu <amy.hailu@ndeverstech.com>
2023-08-28 11:08:03 -0400

    Edited sushi page

--- src/routes/japanese/sushi/+page.svelte
+++ src/routes/japanese/sushi/+page.svelte
@@ -1,10 +1,8 @@
 <h1>Sushi-Zen</h1>
 
-<img src="https://files.brannan.cloud/recipes/sushi.png" alt="Sushi"/>
+<img src="https://files.brannan.cloud/recipes/sushi.png" alt="Sushi" />
 
-<p>
-  This great collection of Sushi Recipes will show you how to make sushi like a pro! 
-</p>
+<p>This great collection of Sushi Recipes will show you how to make sushi like a pro!!!</p>
 
 <h2>Ingredients</h2>
 <ul>
@@ -13,9 +11,9 @@
   <li>2 tablespoons rice vinegar</li>
   <li>2 tablespoons sugar</li>
   <li>1 1/2 teaspoons salt</li>
-  <li> 4 sheets of nori</li>
+  <li>4 sheets of nori</li>
   <li>8 pre-cooked tempura shrimp</li>
-  <li>8 strips of cucumber/li>
+  <li>8 strips of cucumber/li></li>
   <li>8 slices of avocado</li>
   <li>3 tablespoons black and/or white sesame seeds</li>
 </ul>
@@ -24,22 +22,25 @@
 <ol>
   <li>Place the rice into a colander and rinse until the water runs clear.</li>
   <li>
-    Add the rice and 2 cups of water into a medium saucepan over high heat. Bring to a boil, uncovered. Once it begins to boil, reduce the heat to low and cover. Cook for 15 minutes. Remove the pot from the heat and let stand, covered, for 10 minutes.
+    Add the rice and 2 cups of water into a medium saucepan over high heat. Bring to a boil,
+    uncovered. Once it begins to boil, reduce the heat to low and cover. Cook for 15 minutes. Remove
+    the pot from the heat and let stand, covered, for 10 minutes.
   </li>
   <li>
-    Combine the rice vinegar, sugar and salt in a small bowl and heat in the microwave for 20-30 seconds. Transfer the rice to a large bowl and add the vinegar mixture. Fold thoroughly to combine. Allow the rice to cool to room temperature
+    Combine the rice vinegar, sugar and salt in a small bowl and heat in the microwave for 20-30
+    seconds. Transfer the rice to a large bowl and add the vinegar mixture. Fold thoroughly to
+    combine. Allow the rice to cool to room temperature
   </li>
   <li>
-    Place one sheet of nori on a flat surface and press approximately 1/3-1/2 cup rice across the surface of the nori, going all the way to the edges. This is easiest if your fingers are slightly damp.
-  </li>
-  <li>Place a piece of plastic wrap over the rice covered nori and invert on top of a sushi rolling mat so that the seaweed side is up.</li>
-  <li>
-    Place 2 shrimp, 2 strips of cucumber and 2 slices of avocado along one side of the nori.
+    Place one sheet of nori on a flat surface and press approximately 1/3-1/2 cup rice across the
+    surface of the nori, going all the way to the edges. This is easiest if your fingers are
+    slightly damp.
   </li>
   <li>
-    Press approximately 2 teaspoons of sesame seeds into the rice on the sushi roll.
+    Place a piece of plastic wrap over the rice covered nori and invert on top of a sushi rolling
+    mat so that the seaweed side is up.
   </li>
-  <li>
-  Using a sharp knife, slice the sushi then serve immediately.
-  </li>
+  <li>Place 2 shrimp, 2 strips of cucumber and 2 slices of avocado along one side of the nori.</li>
+  <li>Press approximately 2 teaspoons of sesame seeds into the rice on the sushi roll.</li>
+  <li>Using a sharp knife, slice the sushi then serve immediately.</li>
 </ol>
