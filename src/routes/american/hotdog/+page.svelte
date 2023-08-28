d743894a91bc8a968cb3e30959a5662000b7e319
Chubbar5 <connor.hubbard5@gmail.com>
2023-08-28 09:46:32 -0400

    Added the hotdog page. Added the link to the hotdog page to the home page

--- src/routes/american/hotdog/+page.svelte
+++ src/routes/american/hotdog/+page.svelte
@@ -31,7 +31,7 @@ essence of this beloved culinary experience. Get ready to relive the flavors of 
   <li>Chop the onions, tomatoes, and any other toppings you'd like to use, such as pickles or sport peppers. Place them in separate bowls for easy access.</li>
   <li>You can either grill or boil the hotdog sausages. If grilling, preheat the grill to medium-high heat and cook the sausages for about 6-8 minutes,
    turning them occasionally until they're nicely browned. Add the hotdogs and let them cook for about 5 minutes.</li>
-  <li> If you're using a grill, you can lightly toast the hotdog buns for a minute or so. Alternatively, you can wrap them in a damp paper towel and 
+  <li> If you're using a grill, you can lightly toast the hotdog buns for a minute or two. Alternatively, you can wrap them in a damp paper towel and 
   microwave them for about 15-20 seconds to warm them up.</li>
   <li>Place a hotdog sausage in each bun. Now comes the fun part—loading up your hotdog with the classic toppings.</li>
   <li>If you're a fan of cheesy hotdogs, sprinkle some grated cheddar cheese over the toppings.</li>
