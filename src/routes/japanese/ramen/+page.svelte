7a08d015617feea1c46c75dcf75f612786179bd3
spideyIII <sspidel2@gmail.com>
2023-08-29 13:56:36 -0600

    performed prettier script

--- src/routes/japanese/ramen/+page.svelte
+++ src/routes/japanese/ramen/+page.svelte
@@ -1,14 +1,18 @@
 <div>
-    <h1>Ramen</h1>
-    <img
-  src="https://files.brannan.cloud/recipes/ramen.png"
-  alt="A yummy, but definitely greasy, burger."
-/>
-<p>
-    Ramen is a delicious and easy-to-make dish that can satisfy your hunger and cravings. Ramen is a type of Japanese noodle soup that consists of wheat noodles, broth, and various toppings such as meat, eggs, vegetables, and seaweed. Ramen is popular all over the world because of its versatility, affordability, and flavor. You can make ramen at home with simple ingredients and customize it to your liking. 
-</p>
-<h2>Ingredients:</h2>
-<ul>
+  <h1>Ramen</h1>
+  <img
+    src="https://files.brannan.cloud/recipes/ramen.png"
+    alt="A yummy, but definitely greasy, burger."
+  />
+  <p>
+    Ramen is a delicious and easy-to-make dish that can satisfy your hunger and cravings. Ramen is a
+    type of Japanese noodle soup that consists of wheat noodles, broth, and various toppings such as
+    meat, eggs, vegetables, and seaweed. Ramen is popular all over the world because of its
+    versatility, affordability, and flavor. You can make ramen at home with simple ingredients and
+    customize it to your liking.
+  </p>
+  <h2>Ingredients:</h2>
+  <ul>
     <li>4 cups of chicken stock</li>
     <li>2 tablespoons of soy sauce</li>
     <li>1 teaspoon of sugar</li>
@@ -21,12 +25,25 @@
     <li>4 slices of chashu (braised pork belly) or cooked ham</li>
     <li>2 green onions, thinly sliced</li>
     <li>1 sheet of nori (dried seaweed), cut into quarters</li>
-</ul>
-<h2>Directions:</h2>
-<ol>
-    <li>In a large pot, bring the chicken stock, soy sauce, sugar, salt, sesame oil, garlic, and ginger to a boil. Reduce the heat and simmer for about 15 minutes to let the flavors infuse.</li>
-    <li>In a small pot, bring some water to a boil. Carefully lower the eggs into the water and cook for 6 minutes for soft-boiled eggs or 8 minutes for hard-boiled eggs. Transfer the eggs to a bowl of ice water to stop the cooking process. Peel and cut the eggs in half.</li>
-    <li>In another pot, cook the ramen noodles according to the package directions. Drain and rinse with cold water to prevent them from sticking together.</li>
-    <li>To serve, divide the noodles among two large bowls. Ladle the hot broth over the noodles. Top with chashu or ham, eggs, green onions, and nori.</li>
-</ol>
+  </ul>
+  <h2>Directions:</h2>
+  <ol>
+    <li>
+      In a large pot, bring the chicken stock, soy sauce, sugar, salt, sesame oil, garlic, and
+      ginger to a boil. Reduce the heat and simmer for about 15 minutes to let the flavors infuse.
+    </li>
+    <li>
+      In a small pot, bring some water to a boil. Carefully lower the eggs into the water and cook
+      for 6 minutes for soft-boiled eggs or 8 minutes for hard-boiled eggs. Transfer the eggs to a
+      bowl of ice water to stop the cooking process. Peel and cut the eggs in half.
+    </li>
+    <li>
+      In another pot, cook the ramen noodles according to the package directions. Drain and rinse
+      with cold water to prevent them from sticking together.
+    </li>
+    <li>
+      To serve, divide the noodles among two large bowls. Ladle the hot broth over the noodles. Top
+      with chashu or ham, eggs, green onions, and nori.
+    </li>
+  </ol>
 </div>
