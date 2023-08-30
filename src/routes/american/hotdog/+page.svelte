7da7fd90e0937049e0969fc67768e3c85a348b61
Chubbar5 <connor.hubbard5@gmail.com>
2023-08-30 15:26:56 -0400

    Fixed format for hotdog page

--- src/routes/american/hotdog/+page.svelte
+++ src/routes/american/hotdog/+page.svelte
@@ -1,16 +1,13 @@
 <h1>Traditional Ballpark Hotdog</h1>
 
-<img
-  src= "https://files.brannan.cloud/recipes/hotdog.png"
-  alt="Hotdog description"
-/>
+<img src="https://files.brannan.cloud/recipes/hotdog.png" alt="Hotdog description" />
 
 <p>
-  Indulge in the timeless delight of a classic American favorite with our traditional ballpark 
-  hotdog recipe. Step into the nostalgia of sunny afternoons at the ballgame as we guide you 
-  through crafting the perfect hotdog, complete with all the iconic toppings that capture the 
-  essence of this beloved culinary experience. Get ready to relive the flavors of summer and 
-  essence of this beloved culinary experience. Get ready to relive the flavors of summer and 
+  Indulge in the timeless delight of a classic American favorite with our traditional ballpark
+  hotdog recipe. Step into the nostalgia of sunny afternoons at the ballgame as we guide you through
+  crafting the perfect hotdog, complete with all the iconic toppings that capture the essence of
+  this beloved culinary experience. Get ready to relive the flavors of summer and essence of this
+  beloved culinary experience. Get ready to relive the flavors of summer and
 </p>
 
 <h2>Ingredients</h2>
@@ -25,15 +22,29 @@
   <li>Grated cheddar cheese</li>
 </ul>
 
-
 <h2>Instructions</h2>
 <ol>
-  <li>Chop the onions, tomatoes, and any other toppings you'd like to use, such as pickles or sport peppers. Place them in separate bowls for easy access.</li>
-  <li>You can either grill or boil the hotdog sausages. If grilling, preheat the grill to medium-high heat and cook the sausages for about 6-8 minutes,
-   turning them occasionally until they're nicely browned. Add the hotdogs and let them cook for about 5 minutes.</li>
-  <li> If you're using a grill, you can lightly toast the hotdog buns for a minute or two. Alternatively, you can wrap them in a damp paper towel and 
-  microwave them for about 15-20 seconds to warm them up.</li>
-  <li>Place a hotdog sausage in each bun. Now comes the fun part—loading up your hotdog with the classic toppings.</li>
+  <li>
+    Chop the onions, tomatoes, and any other toppings you'd like to use, such as pickles or sport
+    peppers. Place them in separate bowls for easy access.
+  </li>
+  <li>
+    You can either grill or boil the hotdog sausages. If grilling, preheat the grill to medium-high
+    heat and cook the sausages for about 6-8 minutes, turning them occasionally until they're nicely
+    browned. Add the hotdogs and let them cook for about 5 minutes.
+  </li>
+  <li>
+    If you're using a grill, you can lightly toast the hotdog buns for a minute or two.
+    Alternatively, you can wrap them in a damp paper towel and microwave them for about 15-20
+    seconds to warm them up.
+  </li>
+  <li>
+    Place a hotdog sausage in each bun. Now comes the fun part—loading up your hotdog with the
+    classic toppings.
+  </li>
   <li>If you're a fan of cheesy hotdogs, sprinkle some grated cheddar cheese over the toppings.</li>
-  <li>Your traditional ballpark hotdogs are ready to be savored! Serve them up with some crispy potato chips or even nachos for the full stadium experience.</li>
+  <li>
+    Your traditional ballpark hotdogs are ready to be savored! Serve them up with some crispy potato
+    chips or even nachos for the full stadium experience.
+  </li>
 </ol>
