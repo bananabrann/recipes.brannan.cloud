14af7d5e5374d106b31e01d846b5bc4e7840585f
Amy-Hailu <amy.hailu@ndeverstech.com>
2023-10-16 16:36:48 -0400

    edit tamale recipe format

--- src/routes/mexican/tamales/+page.svelte
+++ src/routes/mexican/tamales/+page.svelte
@@ -1,11 +1,12 @@
 <h1>Yummy Tamales</h1>
 
-<img src="https://files.brannan.cloud/recipes/tamales.png" 
-alt="Yummy Tamales."
-/>
+<img src="https://files.brannan.cloud/recipes/tamales.png" alt="Yummy Tamales." />
 
 <p>
-  Tamales are a traditional Mesoamerican dish made of masa (a starchy dough, usually corn-based), which is steamed or boiled in a leaf wrapper, typically a corn husk. The wrapping is discarded before eating. Tamales can be filled with meats, cheeses, fruits, vegetables, chilies, or any preparation according to taste.
+  Tamales are a traditional Mesoamerican dish made of masa (a starchy dough, usually corn-based),
+  which is steamed or boiled in a leaf wrapper, typically a corn husk. The wrapping is discarded
+  before eating. Tamales can be filled with meats, cheeses, fruits, vegetables, chilies, or any
+  preparation according to taste.
 </p>
 
 <h2>Ingredients</h2>
@@ -16,32 +17,51 @@ alt="Yummy Tamales."
   <li>1 tsp baking powder.</li>
   <li>1/2 cup of lard or shortening.</li>
   <li>Salt to taste.</li>
-<h3>For the Filling:</h3>
+  <h3>For the Filling:</h3>
   <li>2 cups of shredded cooked chicken.</li>
   <li>1 cup of salsa verde (green salsa) or a red chili sauce.</li>
   <li>1/2 cup of crumbled queso fresco or another cheese of your choice.</li>
-<h3>Other:</h3>
+  <h3>Other:</h3>
   <li>Corn husks (around 20, soaked in warm water for 30 minutes to an hour to soften).</li>
 </ul>
 
 <h2>Instructions</h2>
 <h3>Prepare the Dough:</h3>
+<ul>
   <li>In a large mixing bowl, whisk together the masa harina, baking powder, and salt.</li>
-  <li>In another bowl, beat the lard or shortening until fluffy. Gradually beat in the masa mixture, then add the chicken broth until a spongy dough forms.</li>
-<h3>Prepare the Corn Husks:</h3>
-  <li>Soak the corn husks in warm water for 30 minutes to an hour to soften them. Drain and set aside..</li>
-<h3>Assemble the Tamales:</h3>  
-  <li>pread a spoonful of the masa dough onto the center of the corn husk.</li>
-  <li>Place a spoonful of the chicken filling in the center of the masa.</li>
-  <li>Fold the sides of the husk in, followed by the top and bottom. You're aiming to wrap the filling in the masa, then wrap the whole tamale in the husk.</li>
-<h3>Cook the Tamales:</h3>  
-  <li>Set up a steamer pot. If you don't have a steamer, you can place a metal colander or strainer over a large regular pot of boiling water. Just ensure the water doesn't touch the tamales.</li>
-  <li>Place the tamales standing up in the steamer or colander.</li>
-  <li>Cover and steam over medium heat for about 60 minutes or until the masa separates easily from the husks.</li>
-<h3>Serve:</h3>  
   <li>
-    Let the tamales cool for a bit before serving. Unwrap them from the corn husks and serve with additional salsa, cheese, and other desired toppings.
+    In another bowl, beat the lard or shortening until fluffy. Gradually beat in the masa mixture,
+    then add the chicken broth until a spongy dough forms.
+  </li>
+</ul>
+<h3>Prepare the Corn Husks:</h3>
+<li>
+  Soak the corn husks in warm water for 30 minutes to an hour to soften them. Drain and set aside..
+</li>
+<h3>Assemble the Tamales:</h3>
+<li>pread a spoonful of the masa dough onto the center of the corn husk.</li>
+<li>Place a spoonful of the chicken filling in the center of the masa.</li>
+<li>
+  Fold the sides of the husk in, followed by the top and bottom. You're aiming to wrap the filling
+  in the masa, then wrap the whole tamale in the husk.
+</li>
+<h3>Cook the Tamales:</h3>
+<li>
+  Set up a steamer pot. If you don't have a steamer, you can place a metal colander or strainer over
+  a large regular pot of boiling water. Just ensure the water doesn't touch the tamales.
+</li>
+<li>Place the tamales standing up in the steamer or colander.</li>
+<li>
+  Cover and steam over medium heat for about 60 minutes or until the masa separates easily from the
+  husks.
+</li>
+<h3>Serve:</h3>
+<li>
+  Let the tamales cool for a bit before serving. Unwrap them from the corn husks and serve with
+  additional salsa, cheese, and other desired toppings.
 </li>
 
-<p>Enjoy your homemade tamales! This is a basic recipe, but tamales are versatile, and you can experiment with different fillings and sauces according to your preference.</p>
-
+<p>
+  Enjoy your homemade tamales! This is a basic recipe, but tamales are versatile, and you can
+  experiment with different fillings and sauces according to your preference.
+</p>
