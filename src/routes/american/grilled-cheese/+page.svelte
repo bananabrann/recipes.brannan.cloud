2c6718388e48ee90294214aa6210cbed502650f0
Chubbar5 <connor.hubbard5@gmail.com>
2023-10-27 13:46:44 -0400

    Added grilled cheese recipe and added link to grilled cheese page

---
+++ src/routes/american/grilled-cheese/+page.svelte
@@ -0,0 +1,34 @@
+<h1>Grilled Cheese</h1>
+
+<img src="https://placehold.co/400x280" alt="A delicious grilled cheese." />
+
+
+
+<h2>Ingredients</h2>
+<ul>
+  <li>4 slices of white or whole wheat bread</li>
+  <li>4 slices of American cheese (or cheddar, if preferred)</li>
+  <li>2 tablespoons of butter, softened</li>
+  <li>Optional: sliced tomatoes, cooked bacon, or pickles for added flavor</li>
+</ul>
+
+<h2>Instructions</h2>
+<ol>
+  <li>Preheat a skillet or griddle over medium heat.</li>
+  <li>Spread butter on one side of each slice of bread.</li>
+  <li>
+    Place one slice of cheese (and any optional ingredients, if desired) between two slices of bread
+    with the buttered side out.
+  </li>
+  <li>
+    Place the sandwich on the skillet, and cook until the bread is golden brown and the cheese is
+    melted, about 2-3 minutes per side.
+  </li>
+  <li>Repeat with the remaining ingredients.</li>
+  <li>Remove from the skillet and let it cool for a minute. Slice in half, and serve warm.</li>
+</ol>
+
+<p>
+  Enjoy your classic American grilled cheese sandwich! Pair with tomato soup for an even more
+  quintessential American experience.
+</p>
